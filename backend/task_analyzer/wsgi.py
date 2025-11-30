91367b723b4877347877c9188ea3ff0c08ace33b
jayasurya0255 <jayasurya0255@gmail.com>
2025-11-30 20:17:32 +0530

    Update wsgi.py

--- backend/task_analyzer/wsgi.py
+++ backend/task_analyzer/wsgi.py
@@ -7,6 +7,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_analyzer.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.task_analyzer.settings')
 
 application = get_wsgi_application()
